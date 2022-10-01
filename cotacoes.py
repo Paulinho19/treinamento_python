1ed1baba2de58dd7f81442d00c7327dd59aac39e
Paulo <paulohsantosrocha@gmail.com>
2022-10-01 08:50:59 -0300

    Update cotacoes.py

--- cotacoes.py
+++ cotacoes.py
@@ -3,12 +3,13 @@ import json
 #Informa as atuais cotações do dólar, euro e bit-coin
 cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 cotacoes = cotacoes.json()
-cotacao_dolar = cotacoes['USDBRL']['bid']
-cotacao_euro = cotacoes['EURBRL']['bid']
-cotacao_bit_coin = cotacoes['BTCBRL']['bid']
-print('1 dolar vale: R$',cotacao_dolar,'reais!')
+cotacao_dolar = float(cotacoes['USDBRL']['bid'])
+cotacao_euro = float(cotacoes['EURBRL']['bid'])
+cotacao_bit_coin = float(cotacoes['BTCBRL']['bid'])
 print('-'*45)
-print('1 euro vale: R$',cotacao_euro,'reais!')
+print('1 dolar vale: R${:.2f} reais!'.format(cotacao_dolar))
 print('-'*45)
-print('1 bit-coin vale: R$',cotacao_bit_coin,'reais!')
+print('1 euro vale: R${:.2f} reais!'.format(cotacao_euro))
+print('-'*45)
+print('1 bit-coin vale: R${:.2f} reais!'.format(cotacao_bit_coin))
 print('-'*45)
