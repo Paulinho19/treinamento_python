f174ea48cf13a4fb8b8752d37e0b5443edd90d3e
Paulo <paulohsantosrocha@gmail.com>
2022-10-09 15:23:02 -0300

    main

---
+++ nome_ao_contrario.py
@@ -0,0 +1,6 @@
+# informa o nome do usuário ao contrário
+nome = str(input('Informe seu nome: ')).strip()
+print('-'*50)
+print('Nome normal: {}'.format(nome))
+print('Nome ao contrário: {}'.format(nome[::-1]))
+print('-'*50)
---
+++ tem_no_nome.py
@@ -0,0 +1,3 @@
+# informa se o nome do usuário possui "Silva"
+nome = str(input('Qual seu nome completo: ')).title()
+print('Seu nome tem Silva? {}'.format('Silva' in nome))
