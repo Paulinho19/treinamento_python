d92cb1cefcb4e7274a60634d7ddc9eb1bdff58df
Paulo <paulohsantosrocha@gmail.com>
2022-10-12 13:16:47 -0300

    Update tabuada_de_multiplicacao.py

--- tabuada_de_multiplicacao.py
+++ tabuada_de_multiplicacao.py
@@ -1,9 +1,10 @@
 # Informa a tabuada de multiplicação do valor apresentado de 0 à 10000
-n1 = float(input('Informe o número '))
-n2 = 0
-resultados = {}
-for i in range (10001):
-    print('{:.0f} x {} = {:.2f}'.format(n1,n2,n1*n2))
-    resultados[n2] = n1*n2
-    n2+=1
-print(resultados)
+continua = 's'
+while continua == 's':
+    n1 = float(input('Informe o número: '))
+    n2 = 0
+    for i in range (201):
+        print('{} x {} = {:.2f}'.format(n1,n2,n1*n2))
+        n2+=1
+    print()
+    continua = input('Deseja continuar (s/n): ')
